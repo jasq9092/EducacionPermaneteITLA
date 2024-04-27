94db6304f6deb761dba1afdf73cd0410b0aa0dd7
Jully Anderson Salas Quezada <20187057@itla.edu.do>
2024-04-27 18:57:07 -0400

    Agregue el codigo del calculo del sueldo con un condicional IF y el try catch.

--- CalculandoSueldo/Program.cs
+++ CalculandoSueldo/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using CalculandoSueldo;
+
+Sueldo sueldo = new Sueldo();
+sueldo.CalculandoSueldo();
---
+++ CalculandoSueldo/Sueldo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculandoSueldo
+{
+    internal class Sueldo
+    {
+        public void CalculandoSueldo() {
+            int HorasTrabajadas;
+            int CostoHoras;
+            int Sueldo;
+            decimal Itbs = 0.18m;
+            decimal ItbsSueldo;
+            try
+            {
+                Console.WriteLine("Ingrese las horas trabajas: ");
+                HorasTrabajadas = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Ingrese el costo de las horas: ");
+                CostoHoras = Convert.ToInt32(Console.ReadLine());
+
+
+                Sueldo = HorasTrabajadas * CostoHoras;
+
+                if (Sueldo > 5000)
+                {
+                    Console.WriteLine("-------------------------Detalles-----------------------------------");
+                    Console.WriteLine("Su sueldo es: " + Sueldo);
+                    ItbsSueldo = Sueldo * Itbs;
+                    Console.WriteLine("Impuestos a pagar: " + ItbsSueldo);
+                    Console.WriteLine($"Su sueldo neto es: {Sueldo - ItbsSueldo}");
+
+
+                }
+                else {
+
+                    Console.WriteLine("-------------------------Detalles-----------------------------------");
+
+                    Console.WriteLine("El sueldo es: " + Sueldo);
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Su programa se detuvo por la siguiente razon: "+ex.Message);
+            }
+            
+
+
+        }
+
+
+    }
+}
