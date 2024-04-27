9846eaed5b60473fcfa5a9e875d79eea838975cc
Jully Anderson Salas Quezada <20187057@itla.edu.do>
2024-04-27 17:43:35 -0400

    Le agregue el "try catch".

--- Practica1/SumaProducto.cs
+++ Practica1/SumaProducto.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,19 +17,28 @@ namespace Practica1
             int suma;
             int producto;
 
-            Console.WriteLine("Este programa realiza la suma y la multiplicacion de dos numeros ingresados por pantalla");
-            Console.WriteLine("---------------------------------------------------------------");
-            Console.WriteLine("Ingrese un numero: ");
-            num1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Ingrese otro numero: ");
-            num2 = Convert.ToInt32(Console.ReadLine());
+            
 
-            suma = num1 + num2;
-            Console.WriteLine("La suma de los numeros es: "+ suma);
+            try
+            {
+                Console.WriteLine("Este programa realiza la suma y la multiplicacion de dos numeros ingresados por pantalla");
+                Console.WriteLine("---------------------------------------------------------------");
+                Console.WriteLine("Ingrese un numero: ");
+                num1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Ingrese otro numero: ");
+                num2 = Convert.ToInt32(Console.ReadLine());
 
-            producto = num1 * num2;
-            Console.WriteLine("El producto de los numeros es: " + producto);
+                suma = num1 + num2;
+                Console.WriteLine("La suma de los numeros es: " + suma);
 
+                producto = num1 * num2;
+                Console.WriteLine("El producto de los numeros es: " + producto);
+            }
+            catch(Exception ex) { 
+
+                Console.WriteLine($"El programa se detuvo por la siguiente razon: {ex.Message}");
+            
+            }
 
 
         }
